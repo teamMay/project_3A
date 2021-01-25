ba21de296ea44d960cb6cf98a2881be3e0af5805
mmehdi21 <38988556+mmehdi21@users.noreply.github.com>
2021-01-25 19:19:40 +0100

    Adding preprocessing and prediction
    Require googletrans API (pip install googletrans==3.1.0a0)

--- app/services/sentiment_analysis/main.py
+++ app/services/sentiment_analysis/main.py
@@ -1,10 +1,27 @@
 from entities.sentiment_analysis import SentimentAnalysisInput, SentimentAnalysisOutput
+import joblib
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegressionCV
+from googletrans import Translator
+
+def translateToEng(sentence):
+  translator = Translator()
+  return(translator.translate(sentence, src='fr').text)
 
 
 def distress_detector(input: SentimentAnalysisInput) -> SentimentAnalysisOutput:
-    # Hello darkness my old friend
-    # PLACE YOUR MODEL CALL THERE
-    if "déprimée" in input.text:
-        return {"label": "Distress", "confidence": 0.99}
+    
+    vectorizer = CountVectorizer(analyzer = "word", 
+                             preprocessor = None,  
+                             max_features = 6000, ngram_range=(1,5))
+    eng_input = vectorizer.transform([translateToEng(input)])
+    
+    loaded_model = joblib.load('LR_model.sav')
+	prediction = loaded_model.predict(eng_input)[0]
+	confidence = round(loaded_model.predict_proba(eng_input)[int(result)],2)
+	if prediction == 0:
+		label = 'Distressed'
+	else:
+		label = 'Calm'
 
-    return {"label": "Neutral", "confidence": 0.75}
+    return {"label": label, "confidence": confidence}
