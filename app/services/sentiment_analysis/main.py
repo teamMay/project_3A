e496870160f8ad84b278ec7684eea45e47536921
mmehdi21 <38988556+mmehdi21@users.noreply.github.com>
2021-01-25 19:41:23 +0100

    Update main.py

--- app/services/sentiment_analysis/main.py
+++ app/services/sentiment_analysis/main.py
@@ -5,22 +5,18 @@ from sklearn.linear_model import LogisticRegressionCV
 from googletrans import Translator
 
 def translateToEng(sentence):
-  translator = Translator()
-  return(translator.translate(sentence, src='fr').text)
+	translator = Translator()
+	return(translator.translate(sentence, src='fr').text)
 
 
 def distress_detector(input: SentimentAnalysisInput) -> SentimentAnalysisOutput:
-    
-    vectorizer = CountVectorizer(analyzer = "word", 
-                             preprocessor = None,  
-                             max_features = 6000, ngram_range=(1,5))
-    eng_input = vectorizer.transform([translateToEng(input)])
-    loaded_model = joblib.load('LR_model.sav')
-    prediction = loaded_model.predict(eng_input)[0]
-    confidence = round(loaded_model.predict_proba(eng_input)[int(result)],2)
+	vectorizer = CountVectorizer(analyzer = "word", preprocessor = None, max_features = 6000, ngram_range=(1,5))
+	eng_input = vectorizer.transform([translateToEng(input)])
+	loaded_model = joblib.load('LR_model.sav')
+	prediction = loaded_model.predict(eng_input)[0]
+	confidence = round(loaded_model.predict_proba(eng_input)[int(result)],2)
 	if prediction == 0:
 		label = 'Distressed'
 	else:
 		label = 'Calm'
-
-    return {"label": label, "confidence": confidence}
+	return {"label": label, "confidence": confidence}
