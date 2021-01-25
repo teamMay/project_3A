9a53c5739efd7b1ba2a99be553dc3ccc70787817
mmehdi21 <38988556+mmehdi21@users.noreply.github.com>
2021-01-25 21:04:23 +0100

    Update main.py

--- app/services/sentiment_analysis/main.py
+++ app/services/sentiment_analysis/main.py
@@ -10,9 +10,9 @@ def translateToEng(sentence):
 
 
 def distress_detector(input: SentimentAnalysisInput) -> SentimentAnalysisOutput:
-	vectorizer = joblib.load('tfidf.sav')
+	vectorizer = joblib.load('/services/sentiment_analysis/tfidf.sav')
 	eng_input = vectorizer.transform([translateToEng(input)])
-	loaded_model = joblib.load('LR_model.sav')
+	loaded_model = joblib.load('/services/sentiment_analysis/LR_model.sav')
 	prediction = loaded_model.predict(eng_input)[0]
 	confidence = round(loaded_model.predict_proba(eng_input)[int(result)],2)
 	if prediction == 0:
